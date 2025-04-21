c1d7601f5f10fb89ac719af27d4f49ec08565e06
Luis Fernando Nunes Martins <166927677+Luisitofn06@users.noreply.github.com>
2025-04-21 16:40:11 -0300

    telav1

--- src/greenvolt/static/js/script.js
+++ src/greenvolt/static/js/script.js
@@ -1,1 +1,16 @@
-// Código JS
+// Menu hambúrguer
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  menuIcon.classList.toggle("fa-times");
+});
+
+// Fechar menu ao clicar em um link
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove("active");
+    menuIcon.classList.remove("fa-times");
+  });
+});
